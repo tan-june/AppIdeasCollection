81f21f0155f8fb203ca0d3f85e3a681476cf838c
Tanmay Singhal <43653881+tan-june@users.noreply.github.com>
2024-01-08 20:06:49 -0500

    dollars to cents

--- src/components/ConvertDollartoCents.js
+++ src/components/ConvertDollartoCents.js
@@ -1,32 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Inputer extends React.Component {
+  state = {
+    dollarValue: '',
+    centsValue: ''
+  };
 
+  convertDollar = () => {
+    const dollarValue = parseFloat(this.state.dollarValue) || 0;
 
+    const dollarFloor = Math.floor(dollarValue);
+    const totalCents = ((dollarValue - dollarFloor)*100) + (dollarFloor * 100);
+
+    this.setState({ centsValue: totalCents });
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ dollarValue: e.target.value }, () => {
+      this.convertDollar();
+    });
+  };
 
   render() {
     return (
+      <div className="centered-container">
+        <div className="content-container">
+          <h1 style={{ textAlign: 'center' }}>Dollar to Cents</h1>
+          <h3 style={{ textAlign: 'center', color: 'red' }}>Input Dollar Value</h3>
+          <center>
+            <br></br>
+            <h3 style={{ textAlign: 'center', color: '#7293cc' }}>
+              Dollar:
+            </h3>
+            <input
+              type="text"
+              onChange={this.handleInputChange}
+              value={this.state.dollarValue}
+              className="inputText"
+              placeholder="Dollar"
+            />
+            <h3 style={{ textAlign: 'center', color: '#7293cc' }}>
+              Cents:
+            </h3>
+            <input
+              type="text"
+              value={this.state.centsValue}
+              className="inputText"
+              placeholder="Cents"
+              disabled
+            />
+            <div className="errorBoxVal" id="errorBox"></div>
 
-<div className="centered-container">
-<div className="content-container">
-  <h1 style={{textAlign:'center'}}>Dollar to Cents</h1>
-  <h3 style={{textAlign:'center', color: 'red'}}>Change Value In Either Box</h3>
-  <center>
-    <br></br>
-   <h3 style={{textAlign:'center', color: '#7293cc'}}> Binary:
-    </h3> 
-    <input type="text" onChange={this.convertDollar} id="bin" className="inputText" placeholder='Binary Number'/>
-    <h3 style={{textAlign:'center', color: '#7293cc'}}> Decimal:
-    </h3> 
-    <input type="text" id="dec" className="inputText" placeholder='Decimal Number'/>   
-    <div className='errorBoxVal' id='errorBox'></div> 
-    
-    </center>
-</div>
+            <br />
+            <br />
+            <br />
+            <Link to="/" className="button-62">
+              Home
+            </Link>
+          </center>
+        </div>
       </div>
     );
   }
 }
 
 export default Inputer;
-
--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -7,19 +7,20 @@ const DefaultPage = () => {
       <div className="content-container">
         <h1 style={{textAlign:'center'}}>Choose One of the Mini Projects</h1>
         <h3 style={{color:'blue', textAlign:'center'}}>Choose an option:</h3>
-
+          <center>
             <Link to="/binary-exchange" className="button-62">
               Binary Exchange
             </Link>
             <Link to="/border-radius" className="button-62">
               Border Radius Displayer
             </Link>
+            <Link to="/dollar-to-cents" className="button-62">
+              Dollar to Cents
+            </Link>
             <Link to="/flip-image" className="button-62">
               Flip Image
             </Link>
-            <Link to="/dollar-to-cents" className="button-62">
-              Dollar to Cents ****not done
-            </Link>
+            </center>
       </div>
     </div>
   );
